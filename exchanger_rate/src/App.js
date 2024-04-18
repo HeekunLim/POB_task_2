10b2b4def80b5825584eaee2ac582e19a77e0382
HeekunLim <fanghk555@gmail.com>
2024-04-19 02:20:40 +0900

    스타일 적용

--- exchanger_rate/src/App.js
+++ exchanger_rate/src/App.js
@@ -2,17 +2,11 @@ import React from "react";
 import Axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { setRate, setDate } from "./redux/slice";
+import styled from "styled-components";
 
 function App() {
+  const country = ["USD", "CAD", "KRW", "HKD", "JPY", "CNY"];
   const [cost, setCost] = React.useState(0);
-  const [country, setCountry] = React.useState([
-    "USD",
-    "CAD",
-    "KRW",
-    "HKD",
-    "JPY",
-    "CNY",
-  ]);
   const [choose, setChoose] = React.useState("USD");
   const [choose2, setChoose2] = React.useState("CAD");
 
@@ -60,49 +54,144 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div>
-        <input
-          id="costInput"
-          type="text"
-          dir="rtl"
-          placeholder={choose}
-          onChange={costChanged}
-        />
-        <select id="countryIn" onChange={countryChanged} defaultValue="USD">
-          {country.map((country1) => {
-            return (
-              <option key={country1} value={country1}>
-                {country1}
-              </option>
-            );
+    <StyledAppContainer>
+      <StyledInnerContainer>
+        <StyledInputRow>
+          <input
+            id="costInput"
+            type="text"
+            dir="rtl"
+            placeholder={choose}
+            onChange={costChanged}
+            style={{
+              fontSize: "30px",
+              height: "34px",
+              margin: "5px",
+            }}
+          />
+          <select
+            id="countryIn"
+            onChange={countryChanged}
+            defaultValue="USD"
+            style={{
+              fontSize: "30px",
+              height: "40px",
+              margin: "5px",
+            }}
+          >
+            {country.map((country1) => {
+              return (
+                <option key={country1} value={country1}>
+                  {country1}
+                </option>
+              );
+            })}
+          </select>
+        </StyledInputRow>
+        <StyledButtonRow>
+          {country.map((country2) => {
+            if (country2 !== choose)
+              return (
+                <button
+                  key={country2}
+                  id={country2}
+                  value={country2}
+                  onClick={countryClick}
+                  style={{
+                    fontSize: "30px",
+                    height: "40px",
+                    margin: "5px",
+                  }}
+                >
+                  {country2}
+                </button>
+              );
           })}
-        </select>
-      </div>
-      <div>
-        {country.map((country2) => {
-          if (country2 !== choose)
-            return (
-              <button
-                key={country2}
-                id={country2}
-                value={country2}
-                onClick={countryClick}
-              >
-                {country2}
-              </button>
-            );
-        })}
-      </div>
-      <div>
-        {choose2}: {(cost / rate[choose]) * rate[choose2]}
-      </div>
-      <div>
-        기준일
-        <input id="dateIn" type="date" value={date} onChange={dateChanged} />
-      </div>
-    </div>
+        </StyledButtonRow>
+        <StyledResultRow>
+          <p
+            style={{
+              fontSize: "50px",
+              color: "blueviolet",
+            }}
+          >
+            {choose2 + ":"}&nbsp;
+          </p>
+          <p
+            style={{
+              fontSize: "50px",
+            }}
+          >
+            {(cost / rate[choose]) * rate[choose2]}
+          </p>
+        </StyledResultRow>
+        <StyledDateRow>
+          <p
+            style={{
+              fontSize: "30px",
+            }}
+          >
+            기준일:&nbsp;
+          </p>
+          <input
+            id="dateIn"
+            type="date"
+            value={date}
+            onChange={dateChanged}
+            style={{
+              fontSize: "30px",
+            }}
+          />
+        </StyledDateRow>
+      </StyledInnerContainer>
+    </StyledAppContainer>
   );
 }
 
 export default App;
+
+const StyledAppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const StyledInnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 600px;
+  margin: 50px;
+  border: solid;
+`;
+
+const StyledInputRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const StyledButtonRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const StyledResultRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 300px;
+`;
+
+const StyledDateRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;

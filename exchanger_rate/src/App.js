7bc937fde466f5ae9fa696470a7921dd052e2d38
HeekunLim <fanghk555@gmail.com>
2024-04-18 19:54:59 +0900

    기능 구현 완료, axios는 api 문제로 주석처리

--- exchanger_rate/src/App.js
+++ exchanger_rate/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Axios from "axios";
 
 function App() {
   const [cost, setCost] = React.useState(0);
@@ -10,8 +11,36 @@ function App() {
     "JPY",
     "CNY",
   ]);
+  const [rate, setRate] = React.useState({
+    USD: 1,
+    CAD: 1.37578,
+    KRW: 1373.879772,
+    HKD: 7.830905,
+    JPY: 154.4335,
+    CNY: 7.238402,
+  });
+  const [date, setDate] = React.useState("2022-01-01");
   const [choose, setChoose] = React.useState("USD");
   const [choose2, setChoose2] = React.useState("CAD");
+
+  // React.useEffect(() => {
+  //   try {
+  //     Axios.get(
+  //       `https://api.apilayer.com/exchangerates_data/latest?base=USD&symbols=USD,CAD,KRW,HKD,JPY,CNY&apikey=${process.env.REACT_APP_API_KEY}`,
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     ).then((response) => {
+  //       console.log(response.data.rates);
+
+  //       setRate(response.data.rates);
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }, []);
 
   const costChanged = (e) => {
     e.target.value = Number(e.target.value.replace(/[^0-9]/g, ""));
@@ -19,8 +48,6 @@ function App() {
     e.target.value = e.target.value
       .toString()
       .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-
-    console.log("금액" + cost);
   };
 
   const countryChanged = (e) => {
@@ -28,48 +55,53 @@ function App() {
 
     if (e.target.value === "USD") setChoose2("CAD");
     else setChoose2("USD");
-
-    console.log("선택" + choose);
-    console.log("버튼" + choose2);
   };
 
   const countryClick = (e) => {
     setChoose2(e.target.value);
-
-    console.log("버튼" + choose2);
   };
 
   return (
     <div className="App">
-      <input
-        id="costInput"
-        type="text"
-        placeholder={choose}
-        onChange={costChanged}
-      />
-      <select id="countryIn" onChange={countryChanged} defaultValue="USD">
-        {country.map((country1) => {
-          return (
-            <option key={country1} value={country1}>
-              {country1}
-            </option>
-          );
+      <div>
+        <input
+          id="costInput"
+          type="text"
+          dir="rtl"
+          placeholder={choose}
+          onChange={costChanged}
+        />
+      </div>
+      <div>
+        <select id="countryIn" onChange={countryChanged} defaultValue="USD">
+          {country.map((country1) => {
+            return (
+              <option key={country1} value={country1}>
+                {country1}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div>
+        {country.map((country2) => {
+          if (country2 !== choose)
+            return (
+              <button
+                key={country2}
+                id={country2}
+                value={country2}
+                onClick={countryClick}
+              >
+                {country2}
+              </button>
+            );
         })}
-      </select>
-      {country.map((country2) => {
-        if (country2 !== choose)
-          return (
-            <button
-              key={country2}
-              id={country2}
-              value={country2}
-              onClick={countryClick}
-            >
-              {country2}
-            </button>
-          );
-      })}
-      {choose2}
+      </div>
+      <div>
+        {choose2}: {(cost / rate[choose]) * rate[choose2]}
+      </div>
+      <div>기준일: {date}</div>
     </div>
   );
 }
